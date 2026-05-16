3a1e47e74ecedbd3c566273a70ec20a3ce3f7a5a
BalaVenkataAvinash <balavenkataavinashpillem@gmail.com>
2026-05-16 17:46:48 +0530

    Changes are added in main.py

--- main.py
+++ main.py
@@ -1,16 +1,291 @@
+# from fastapi import FastAPI
+# from contextlib import asynccontextmanager
+
+# # ------------------- CORE -------------------
+# from core.database import Base, engine, SessionLocal
+# from core.seed import create_default_super_admin
+# from core.config import settings
+# from consent.seed import seed_all
+# from utils.db_init import ensure_enum_values
+
+# # ------------------- AUTH -------------------
+# from routers.Auth import lender, superadmin_access, login_SL, user_register, twofactor, email,forgot_password,biometric,support
+# from routers.Auth.superadmin_support_dashboard import router as superadmin_support_router
+# # ------------------- PROFILE & KYC -------------------
+# from routers.Profile_KYC.profile_router import router as profile_router
+# from routers.Profile_KYC.pan_router import router as pan_router
+# from routers.Profile_KYC.aadhaar_router import router as aadhaar_router
+# from routers.Profile_KYC.bank_router import router as bank_router
+# from routers.Profile_KYC.document_router import router as document_router
+# from routers.Profile_KYC.admin_router import router as admin_router
+# from services.Profile_KYC.auto_cleanup import AutoCleanup
+# from routers.Profile_KYC.email_verify_router import router as email_verify_router
+
+
+# # ------------------- CONSENT -------------------
+# from routers.Consent.consent_routers import router as consent_router
+# from routers.Consent.legal_routers import router as legal_router
+
+# # ------------------- SETTINGS -------------------
+# from routers.Settings.profile_router import router as settings_profile_router
+# from routers.Settings.settings_router import router as settings_router
+
+# # ------------------- ELIGIBILITY -------------------
+# from routers.Eligibility.credit_route import router as credit_router
+# from routers.Eligibility.eligibility_route import router as eligibility_router
+# from routers.Eligibility.eligibility_result import router as eligibility_result_router
+# from routers.Eligibility.get_lenders import router as get_lenders_router
+# from routers.Eligibility.select_lender import router as select_lender_router
+# from routers.Eligibility.loan_calculator_route import router as loan_calculator_router
+
+# # ------------------- LOAN APPLICATION -------------------
+# from routers.Loan_application.loan_application_router import router as loan_application_router
+# from routers.Loan_application.loan_application_purpose_router import router as loan_application_purpose_router
+# from routers.Loan_application.loan_application_reference_router import router as loan_application_reference_router
+# from routers.Loan_application.reference_otp_router import router as reference_otp_router
+# from routers.Loan_application.loan_application_declaration_router import router as loan_application_declaration_router
+# from routers.Loan_application.loan_application_summary_router import router as loan_application_summary_router
+# from routers.Loan_application.lender_dashboard_router import router as lender_dashboard_router
+# from routers.Loan_application.razorpayx_webhook import router as razorpayx_webhook_router
+# from routers.Loan_application.user_predisbursement_router import router as user_predisbursement_router
+# from routers.Loan_application.loan_application_submit_router import router as loan_application_submit_router
+# from routers.Loan_application.disbursement_status_router import (
+#     router as disbursement_status_router
+# )
+# # ------------------- TRACKING -------------------
+# from routers.Tracking.tracking_router import router as tracking_router
+# #from routers.Tracking.reupload_router import router as reupload_router
+# from routers.Tracking.document_status_router import router as reupload_router
+# from routers.Tracking.nbfc_webhook_router import router as nbfc_router
+# from routers.Tracking.notifications_router import router as notifications_router
+# from routers.Tracking.internal_status_router import router as internal_status_router
+
+# # ------------------- REPAYMENT -------------------
+# from routers.Repayment.generate_emi import router as generate_emi_router
+# from routers.Repayment.emi_pdf import router as emi_pdf_router
+# from routers.Repayment.emi_reminder import router as emi_reminder_router
+# from routers.Repayment.overdue import router as overdue_router
+# from routers.Repayment.auto_debit import router as auto_debit_router
+# from routers.Repayment.manual import router as manual_router
+# from routers.Repayment.prepay_route import router as prepay_route_router
+# from routers.Repayment.foreclosure import router as foreclosure_router
+# from routers.Repayment.payment_history import router as payment_history_router
+# from routers.Repayment.payment_receipt import router as payment_receipt_router
+# from routers.Repayment.loan_closure import router as loan_closure_router
+# from routers.Repayment.ndc import router as ndc_router
+# from routers.Repayment.webhook import router as razorpay_webhook_router
+# # ------------------- SUPPORT -------------------
+# from routers.Support.faq import router as faq_router
+# from routers.Support.chat import router as chat_router
+# from routers.Support.complaint import router as complaint_router
+# from routers.Support.contact import router as contact_router
+# from routers.Support.support_dashboard import (
+#     router as support_dashboard_router)
+# from routers.Support.complaint_reply import router as complaint_reply_router
+# # ------------------- ESIGN -------------------
+# from routers.Esign.esign_router import router as esign_router
+# from routers.Esign.agreement_router import router as agreement_router
+
+
+
+# auto_cleanup = AutoCleanup(interval_hours=24)
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     Base.metadata.create_all(bind=engine, checkfirst=True)
+
+#     auto_cleanup.start()
+#     print("Auto cleanup service started")
+#     yield
+#     auto_cleanup.stop()
+#     print("Auto cleanup service stopped")
+
+
+# app = FastAPI(
+#     title="Loan Service Platform - API",
+#     lifespan=lifespan
+# )
+
+# # ------------------- STARTUP -------------------
+# # @app.on_event("startup")
+
+# # async def startup():
+ 
+# #     db = SessionLocal()
+ 
+# #     try:
+ 
+# #         print("SUPER_ADMIN_NAME:", settings.SUPER_ADMIN_NAME)
+
+# #         print("SUPER_ADMIN_MOBILE:", settings.SUPER_ADMIN_MOBILE)
+ 
+# #         if settings.SUPER_ADMIN_MOBILE and settings.SUPER_ADMIN_PASSWORD:
+ 
+# #             print("Creating Super Admin...")
+ 
+# #             create_default_super_admin(
+
+# #                 db,
+
+# #                 settings.SUPER_ADMIN_NAME,
+
+# #                 settings.SUPER_ADMIN_MOBILE,
+
+# #                 settings.SUPER_ADMIN_PASSWORD,
+
+# #                 settings.SUPER_ADMIN_DEVICE_ID,
+
+# #             )
+ 
+# #             print("Super Admin Done")
+ 
+# #         seed_all(db)
+ 
+# #     except Exception as e:
+
+# #         print("STARTUP ERROR:", str(e))
+ 
+# #     finally:
+
+# #         db.close()
+
+# @app.on_event("startup")
+# def startup():
+#     Base.metadata.create_all(bind=engine, checkfirst=True)
+
+#     db = SessionLocal()
+
+#     try:
+#         print("SUPER_ADMIN_NAME:", settings.SUPER_ADMIN_NAME)
+#         print("SUPER_ADMIN_MOBILE:", settings.SUPER_ADMIN_MOBILE)
+
+#         create_default_super_admin(
+#             db,
+#             settings.SUPER_ADMIN_NAME,
+#             settings.SUPER_ADMIN_MOBILE,
+#             settings.SUPER_ADMIN_PASSWORD,
+#             settings.SUPER_ADMIN_DEVICE_ID,
+#         )
+
+#         seed_all(db)
+
+#         auto_cleanup.start()
+#         print("Auto cleanup service started")
+
+#     except Exception as e:
+#         print("STARTUP ERROR:", repr(e))
+
+#     finally:
+#         db.close()
+ 
+ 
+
+
+# # ------------------- ROUTES -------------------
+# app.include_router(login_SL.router)
+# app.include_router(user_register.router)
+# app.include_router(lender.router)
+# app.include_router(superadmin_access.router)
+# app.include_router(twofactor.router)
+# app.include_router(email.router)
+# app.include_router(forgot_password.router)
+# app.include_router(biometric.router)
+# app.include_router(support.router)
+# app.include_router(superadmin_support_router)
+
+# app.include_router(profile_router)
+# app.include_router(email_verify_router)
+# app.include_router(pan_router)
+# app.include_router(aadhaar_router)
+# app.include_router(bank_router)
+# app.include_router(document_router)
+# app.include_router(admin_router)
+
+# app.include_router(consent_router)
+# app.include_router(legal_router)
+
+# app.include_router(credit_router, tags=["Credit Profile"])
+# app.include_router(eligibility_router, tags=["Loan Eligibility"])
+# app.include_router(eligibility_result_router, tags=["Loan Eligibility"])
+# app.include_router(get_lenders_router, tags=["Lenders"])
+# app.include_router(select_lender_router, tags=["Lenders"])
+# app.include_router(loan_calculator_router, tags=["Loan Calculator"])
+
+# app.include_router(loan_application_router)
+# app.include_router(loan_application_purpose_router)
+# app.include_router(loan_application_reference_router)
+# app.include_router(reference_otp_router)
+# app.include_router(loan_application_declaration_router)
+# app.include_router(loan_application_summary_router)
+# app.include_router(loan_application_submit_router)
+# app.include_router(user_predisbursement_router)
+# app.include_router(lender_dashboard_router)
+# app.include_router(razorpayx_webhook_router)
+# app.include_router(disbursement_status_router)
+
+# app.include_router(tracking_router)
+# app.include_router(reupload_router)
+# app.include_router(nbfc_router)
+# app.include_router(notifications_router)
+# app.include_router(internal_status_router)
+
+# app.include_router(generate_emi_router)
+# app.include_router(emi_pdf_router)
+# app.include_router(emi_reminder_router)
+# app.include_router(overdue_router)
+# app.include_router(auto_debit_router)
+# app.include_router(manual_router)
+# app.include_router(razorpay_webhook_router)
+# app.include_router(prepay_route_router)
+# app.include_router(foreclosure_router)
+# app.include_router(payment_history_router)
+# app.include_router(payment_receipt_router)
+# app.include_router(loan_closure_router)
+# app.include_router(ndc_router)
+
+# app.include_router(agreement_router)
+# app.include_router(esign_router)
+
+
+# app.include_router(faq_router)
+# app.include_router(chat_router)
+# app.include_router(complaint_router)
+# app.include_router(contact_router)
+# app.include_router(
+#     support_dashboard_router)
+# app.include_router(complaint_reply_router)
+
+# app.include_router(settings_profile_router)
+# app.include_router(settings_router)
+
+
+# # ------------------- ROOT -------------------
+# @app.get("/")
+# def root():
+#     return {"message": "API is running successfully"}
+
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 
 # ------------------- CORE -------------------
 from core.database import Base, engine, SessionLocal
 from core.seed import create_default_super_admin
 from core.config import settings
 from consent.seed import seed_all
-from utils.db_init import ensure_enum_values
 
 # ------------------- AUTH -------------------
-from routers.Auth import lender, superadmin_access, login_SL, user_register, twofactor, email,forgot_password,biometric,support
+from routers.Auth import (
+    lender,
+    superadmin_access,
+    login_SL,
+    user_register,
+    twofactor,
+    email,
+    forgot_password,
+    biometric,
+    support,
+)
 from routers.Auth.superadmin_support_dashboard import router as superadmin_support_router
+
 # ------------------- PROFILE & KYC -------------------
 from routers.Profile_KYC.profile_router import router as profile_router
 from routers.Profile_KYC.pan_router import router as pan_router
@@ -20,7 +295,6 @@ from routers.Profile_KYC.document_router import router as document_router
 from routers.Profile_KYC.admin_router import router as admin_router
 from services.Profile_KYC.auto_cleanup import AutoCleanup
 from routers.Profile_KYC.email_verify_router import router as email_verify_router
-
 
 # ------------------- CONSENT -------------------
 from routers.Consent.consent_routers import router as consent_router
@@ -49,12 +323,10 @@ from routers.Loan_application.lender_dashboard_router import router as lender_da
 from routers.Loan_application.razorpayx_webhook import router as razorpayx_webhook_router
 from routers.Loan_application.user_predisbursement_router import router as user_predisbursement_router
 from routers.Loan_application.loan_application_submit_router import router as loan_application_submit_router
-from routers.Loan_application.disbursement_status_router import (
-    router as disbursement_status_router
-)
+from routers.Loan_application.disbursement_status_router import router as disbursement_status_router
+
 # ------------------- TRACKING -------------------
 from routers.Tracking.tracking_router import router as tracking_router
-#from routers.Tracking.reupload_router import router as reupload_router
 from routers.Tracking.document_status_router import router as reupload_router
 from routers.Tracking.nbfc_webhook_router import router as nbfc_router
 from routers.Tracking.notifications_router import router as notifications_router
@@ -74,84 +346,34 @@ from routers.Repayment.payment_receipt import router as payment_receipt_router
 from routers.Repayment.loan_closure import router as loan_closure_router
 from routers.Repayment.ndc import router as ndc_router
 from routers.Repayment.webhook import router as razorpay_webhook_router
+
 # ------------------- SUPPORT -------------------
 from routers.Support.faq import router as faq_router
 from routers.Support.chat import router as chat_router
 from routers.Support.complaint import router as complaint_router
 from routers.Support.contact import router as contact_router
-from routers.Support.support_dashboard import (
-    router as support_dashboard_router)
+from routers.Support.support_dashboard import router as support_dashboard_router
 from routers.Support.complaint_reply import router as complaint_reply_router
+
 # ------------------- ESIGN -------------------
 from routers.Esign.esign_router import router as esign_router
 from routers.Esign.agreement_router import router as agreement_router
 
 
-
 auto_cleanup = AutoCleanup(interval_hours=24)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    Base.metadata.create_all(bind=engine, checkfirst=True)
-
-    auto_cleanup.start()
-    print("Auto cleanup service started")
-    yield
-    auto_cleanup.stop()
-    print("Auto cleanup service stopped")
-
-
 app = FastAPI(
-    title="Loan Service Platform - API",
-    lifespan=lifespan
+    title="Loan Service Platform - API"
 )
 
+
 # ------------------- STARTUP -------------------
-# @app.on_event("startup")
-
-# async def startup():
- 
-#     db = SessionLocal()
- 
-#     try:
- 
-#         print("SUPER_ADMIN_NAME:", settings.SUPER_ADMIN_NAME)
-
-#         print("SUPER_ADMIN_MOBILE:", settings.SUPER_ADMIN_MOBILE)
- 
-#         if settings.SUPER_ADMIN_MOBILE and settings.SUPER_ADMIN_PASSWORD:
- 
-#             print("Creating Super Admin...")
- 
-#             create_default_super_admin(
-
-#                 db,
-
-#                 settings.SUPER_ADMIN_NAME,
-
-#                 settings.SUPER_ADMIN_MOBILE,
-
-#                 settings.SUPER_ADMIN_PASSWORD,
-
-#                 settings.SUPER_ADMIN_DEVICE_ID,
-
-#             )
- 
-#             print("Super Admin Done")
- 
-#         seed_all(db)
- 
-#     except Exception as e:
-
-#         print("STARTUP ERROR:", str(e))
- 
-#     finally:
-
-#         db.close()
-
 @app.on_event("startup")
 def startup():
+    print("🚀 Starting application...")
+
     Base.metadata.create_all(bind=engine, checkfirst=True)
+    print("✅ Tables created successfully")
 
     db = SessionLocal()
 
@@ -159,7 +381,7 @@ def startup():
         print("SUPER_ADMIN_NAME:", settings.SUPER_ADMIN_NAME)
         print("SUPER_ADMIN_MOBILE:", settings.SUPER_ADMIN_MOBILE)
 
-        create_default_super_admin(
+        admin = create_default_super_admin(
             db,
             settings.SUPER_ADMIN_NAME,
             settings.SUPER_ADMIN_MOBILE,
@@ -167,18 +389,27 @@ def startup():
             settings.SUPER_ADMIN_DEVICE_ID,
         )
 
+        print("✅ SUPER ADMIN READY:", admin.mobile_number if admin else None)
+
         seed_all(db)
+        print("✅ Consent seed completed")
 
         auto_cleanup.start()
-        print("Auto cleanup service started")
+        print("✅ Auto cleanup service started")
 
     except Exception as e:
-        print("STARTUP ERROR:", repr(e))
+        db.rollback()
+        print("❌ STARTUP ERROR:", repr(e))
 
     finally:
         db.close()
- 
- 
+
+
+# ------------------- SHUTDOWN -------------------
+@app.on_event("shutdown")
+def shutdown():
+    auto_cleanup.stop()
+    print("🛑 Auto cleanup service stopped")
 
 
 # ------------------- ROUTES -------------------
@@ -246,13 +477,11 @@ app.include_router(ndc_router)
 app.include_router(agreement_router)
 app.include_router(esign_router)
 
-
 app.include_router(faq_router)
 app.include_router(chat_router)
 app.include_router(complaint_router)
 app.include_router(contact_router)
-app.include_router(
-    support_dashboard_router)
+app.include_router(support_dashboard_router)
 app.include_router(complaint_reply_router)
 
 app.include_router(settings_profile_router)
